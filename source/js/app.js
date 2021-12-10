99150f3f0ed44ab136bc0aaf06da231ebc2bda41
igorgaev2014 <igorgaev2014@gmail.com>
2021-12-10 23:21:25 +0300

    Начало верстки шапки

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,16 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+const pageHeader = document.querySelector('.page-header');
+const headerToggle = document.querySelector('.main-nav__button');
+
+// pageHeader.classList.remove('page-header--nojs');
 
 headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--closed')) {
     pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
+    console.log('clicked');
+
   } else {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
